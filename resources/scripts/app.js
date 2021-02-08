9cb5f3a665354f8de8dc071d40a69f2a53e18cb5
Wolfgang Meier <wolfgang@existsolutions.com>
2021-02-08 12:30:48 +0100

    Pass SVG path upon load

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const sites = document.getElementById('sites');
-    const documents = document.getElementById('documents');
     pbEvents.subscribe('pb-end-update', 'sites', () => {
+        const sites = document.getElementById('sites');
+        const documents = document.getElementById('documents');
         pbEvents.emit('pb-update', 'map', {
             root: sites
         });
@@ -13,5 +13,22 @@ window.addEventListener('DOMContentLoaded', () => {
                 });
             });
         });
+    });
+
+    const panel = document.querySelector('pb-panel');
+    let svgPath;
+    pbEvents.subscribe('pb-update', 'catalog', (ev) => {
+        const svgImg = ev.detail.root.querySelector('.layout');
+        svgPath = svgImg.getAttribute('src');
+        const svg = panel.querySelector('pb-svg');
+        if (svg) {
+            svg.setAttribute('url', svgPath);
+        }
+    });
+    pbEvents.subscribe('pb-panel', 'panels', () => {
+        const svg = panel.querySelector('pb-svg');
+        if (svg) {
+            svg.setAttribute('url', svgPath);
+        }
     });
 });
